dae0b1158e74b88bf4d5dbdf17fe1a3bc357dc96
Tian Pengfei <itsneo1990@outlook.com>
2018-02-07 14:00:43 +0800

    serializer fields

--- {{cookiecutter.project_slug}}/api/serializers.py
+++ {{cookiecutter.project_slug}}/api/serializers.py
@@ -6,3 +6,4 @@ from rest_framework import serializers
 class {{cookiecutter.default_model}}Serializer(serializers.ModelSerializer):
     class Meta:
         model = {{cookiecutter.default_model}}
+        fields = "__all__"
