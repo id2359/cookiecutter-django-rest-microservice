05fb17888547867f48b9673205e60783d7f934c6
Tian Pengfei <itsneo1990@outlook.com>
2018-02-07 12:05:09 +0800

    views fix

--- {{cookiecutter.project_slug}}/api/views.py
+++ {{cookiecutter.project_slug}}/api/views.py
@@ -2,7 +2,9 @@
 from api.models import {{cookiecutter.default_model}}
 from rest_framework import viewsets
 from api.permissions import {{cookiecutter.default_model}}Permission
+from api.serializers import {{cookiecutter.default_model}}Serializer
 
 class {{cookiecutter.default_model}}ViewSet(viewsets.ModelViewSet):
-    queryset = {{cookiecutter.default_model}}
+    queryset = {{cookiecutter.default_model}}.objects.all()
+    serializer_class = {{cookiecutter.default_model}}Serializer
     permission_classes = [{{cookiecutter.default_model}}Permission, ]
